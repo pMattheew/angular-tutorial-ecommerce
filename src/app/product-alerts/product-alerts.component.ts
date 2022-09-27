5c53cda7f526fcfd0de0667623f3102d91b6bd3e
pMattheew <99044724+pMattheew@users.noreply.github.com>
2022-09-27 19:57:56 -0300

    event handling and input/output between parents and childs components

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -8,7 +8,8 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
 
-  @Input() product!: Product;
+  @Input() product!: Product | undefined;
+  @Output() notify = new EventEmitter();
 
   constructor() { }
 
